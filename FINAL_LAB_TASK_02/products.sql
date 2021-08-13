a85c39a864e8e40c98bfadac2128065928e9cb33
tanvirtanjumnafis <77985381+tanvirtanjumnafis@users.noreply.github.com>
2021-08-13 21:32:34 +0600

    finallabtask2

---
+++ FINAL_LAB_TASK_02/products.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2021 at 03:52 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `product_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `buyingPrice` int(50) NOT NULL,
+  `sellingPrice` int(50) NOT NULL,
+  `displayable` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `buyingPrice`, `sellingPrice`, `displayable`) VALUES
+(0, 'Nokia', 1800, 1500, 'Yes'),
+(0, 'Xiaomi', 2000, 3000, 'No'),
+(0, 'Samsung', 120000, 150000, 'No');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
